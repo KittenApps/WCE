50cae91631f10ae92b6f80d6b322268af767d1a8
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-04-12 13:47:07 +0200

    add userscript metadata

--- fbc.user.js
+++ fbc.user.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @name FBC fork
+// @namespace https://www.bondageprojects.com/
+// @version 5.8.1
+// @description fork of old fbc
+// @author Sidious (and others)
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
 /* eslint-disable no-inline-comments */
 // @ts-check
 
