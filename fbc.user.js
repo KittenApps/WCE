d1a495484ffe9a446f5aff62865d0eecd092c374
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-09 15:10:01 +0200

    misc fixes

--- fbc.user.js
+++ fbc.user.js
@@ -160,6 +160,11 @@ const defaultSettings = /** @type {const} */ ({
         // Disable conflicting settings
         Player.ArousalSettings.AffectExpression = false;
       }
+      if (!newValue) {
+        fbcSettings.expressions = false;
+        fbcSettings.activityExpressions = false;
+      }
+      debug("animationEngine", newValue);
     },
     value: false,
     category: "activities",
@@ -238,12 +243,12 @@ const defaultSettings = /** @type {const} */ ({
   },
   allowLayeringOthers: {
     label: "Enable layering menus on other players",
-    value: false,
+    value: true,
     /**
      * @param {unknown} newValue
      */
     sideEffects: (newValue) => {
-      debug("layeringOthers", newValue);
+      debug("allowLayeringOthers", newValue);
     },
     category: "appearance",
     description: "Let's you edit the layering on items of other players.",
@@ -1017,25 +1022,19 @@ function displayText(original, replacements = {}) {
       "Use italics for input when whispering": "悄悄话使用斜体字",
       "Improve colors for readability": "改善颜色以提高可读性",
       "Show friend presence notifications": "显示好友在线通知",
-      "Show friends going offline too (requires friend presence)": "显示朋友离线通知 (需要启用好友在线通知)",
       "Understand All Gagged and when Deafened": "在被堵住嘴和被堵住耳朵时可以听懂所有发言",
       "Reveal Lockpicking Order Based on Skill": "根据技能显示撬锁/开锁顺序",
       "Allow layering menus while bound": "允许在捆绑时用分层菜单",
-      "Load BCX by Jomshir98 (requires refresh - no auto-update)": "加载 BCX by Jomshir98 (需要刷新 - 无自动更新)",
-      "Load BCX beta (requires refresh - auto-updates, compatibility not guaranteed)":
-        "加载 BCX beta 测试版 (需要刷新 - 自动升级, 不保证兼容性)",
       "Limited gag anti-cheat: cloth-gag equivalent garbling": "有限的堵嘴反作弊: 和布堵嘴相同的乱码",
       "Full gag anti-cheat: use equipped gags to determine garbling": "完整的堵嘴反作弊: 使用当前装备的堵嘴来确定乱码",
       "Extra gag anti-cheat: even more garbling for the most extreme gags":
         "扩展的堵嘴反作弊: 对于使用最极端的堵嘴更加混乱",
       "Require glasses to see": "需要眼镜才能看清",
-      "Check for updates": "检查更新",
       "Automatic Relogin on Disconnect": "断线后自动重连",
       "Show gag cheat and anti-cheat options in chat": "在聊天室里显示堵嘴作弊和反作弊选项",
       "Automatically ghost+blocklist unnaturally new users": "自动对不自然的用户无视并添加黑名单",
       "Confirm leaving the game": "离开游戏前需要确认",
       "Discreet mode (disable drawing)": "谨慎模式 (禁用绘图)",
-      "Keep tab active (requires refresh)": "保持标签页处于活动状态 (需要刷新)",
       "Make automatic progress while struggling": "在挣扎时自动增加进度",
       "Allow leashing without wearing a leashable item (requires leasher to have FBC too)":
         "允许在不佩戴牵引绳的情况下也可以进行牵引（需要牵引者也安装有FBC）",
@@ -1050,9 +1049,6 @@ function displayText(original, replacements = {}) {
       Performance: "表现",
       Misc: "杂项",
       Cheats: "作弊",
-      "Other Addons": "其他插件",
-      "Show nicknames": "修改你的昵称",
-      "Change your nickname": "修改你的昵称",
       ah: "啊",
       aah: "啊❤",
       mnm: "唔姆",
@@ -1131,9 +1127,6 @@ function displayText(original, replacements = {}) {
       Import: "导入",
       Export: "导出",
       "Description:": "描述:",
-      Submit: "提交",
-      Cancel: "取消",
-      "Click to close the modal": "点击关闭情态",
       "Animation Engine": "动画引擎",
       "Show numeric arousal meter": "显示欲望条数值",
       "Show friends going offline too": "显示朋友离线通知",
@@ -2263,7 +2256,6 @@ async function settingsPage() {
     performance: "Performance",
     misc: "Misc",
     cheats: "Cheats",
-    addons: "Other Addons",
     buttplug: "Buttplug.io Toys",
     hidden: "",
   });
@@ -3338,6 +3330,24 @@ async function automaticExpressions() {
   if (!Player.ArousalSettings) {
     throw new Error("Player.ArousalSettings is not defined");
   }
+
+  patchFunction(
+    "PreferenceSubscreenArousalRun",
+    {
+      'DrawCheckbox(1250, 276, 64, 64, TextGet("ArousalAffectExpression"), Player.ArousalSettings.AffectExpression);': 
+        'DrawCheckbox(1250, 276, 64, 64, TextGet("ArousalAffectExpression"), Player.ArousalSettings.AffectExpression, fbcSettingValue("animationEngine"));',
+    },
+    "disabling conflicting Player.ArousalSettings.AffectExpression when Animation Engine is active"
+  );
+
+  patchFunction(
+    "PreferenceSubscreenArousalClick",
+    {
+      'Player.ArousalSettings.AffectExpression = !Player.ArousalSettings.AffectExpression;': 
+        'Player.ArousalSettings.AffectExpression = !Player.ArousalSettings.AffectExpression && !fbcSettingValue("animationEngine");',
+    },
+    "disabling conflicting Player.ArousalSettings.AffectExpression when Animation Engine is active"
+  );
 
   patchFunction(
     "StruggleMinigameHandleExpression",
