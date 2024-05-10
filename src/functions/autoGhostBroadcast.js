a2f848aa6c4c1b92206028de1e908ac16c486e19
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-11 01:39:17 +0200

    split up functions/autoGhostBroadcast

---
+++ src/functions/autoGhostBroadcast.js
@@ -0,0 +1,27 @@
+import { registerSocketListener } from "..";
+import { waitFor } from "../util/util";
+import { debug } from "../util/logger";
+import { fbcSettings } from "../util/settings";
+
+export async function autoGhostBroadcast() {
+  await waitFor(() => !!ServerSocket && ServerIsConnected);
+  registerSocketListener("ChatRoomSyncMemberJoin", (data) => {
+    if (fbcSettings.ghostNewUsers && Date.now() - data.Character.Creation < 30000) {
+      ChatRoomListManipulation(Player.BlackList, true, data.Character.MemberNumber.toString());
+      if (!Player.GhostList) {
+        Player.GhostList = [];
+      }
+      ChatRoomListManipulation(Player.GhostList, true, data.Character.MemberNumber.toString());
+      debug(
+        "Blacklisted",
+        data.Character.Name,
+        // @ts-ignore - CharacterNickname works with this too
+        CharacterNickname(data.Character),
+        data.Character.MemberNumber,
+        "registered",
+        (Date.now() - data.Character.Creation) / 1000,
+        "seconds ago"
+      );
+    }
+  });
+}
--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ import { privateWardrobe } from './functions/privateWardrobe';
 import { antiGarbling } from './functions/antiGarbling';
 import { automaticExpressions } from './functions/automaticExpressions';
 import { alternateArousal } from './functions/alternateArousal';
+import { autoGhostBroadcast } from './functions/autoGhostBroadcast';
 
 await waitFor(() => typeof FUSAM === "object" && FUSAM?.present && typeof bcModSdk === "object" && !!bcModSdk);
 
@@ -523,29 +524,6 @@ createTimer(() => {
     sendHello(null, true);
   }
 }, 5000);
-
-async function autoGhostBroadcast() {
-  await waitFor(() => !!ServerSocket && ServerIsConnected);
-  registerSocketListener("ChatRoomSyncMemberJoin", (data) => {
-    if (fbcSettings.ghostNewUsers && Date.now() - data.Character.Creation < 30000) {
-      ChatRoomListManipulation(Player.BlackList, true, data.Character.MemberNumber.toString());
-      if (!Player.GhostList) {
-        Player.GhostList = [];
-      }
-      ChatRoomListManipulation(Player.GhostList, true, data.Character.MemberNumber.toString());
-      debug(
-        "Blacklisted",
-        data.Character.Name,
-        // @ts-ignore - CharacterNickname works with this too
-        CharacterNickname(data.Character),
-        data.Character.MemberNumber,
-        "registered",
-        (Date.now() - data.Character.Creation) / 1000,
-        "seconds ago"
-      );
-    }
-  });
-}
 
 async function blindWithoutGlasses() {
   await waitFor(() => !!Player && !!Player.Appearance);
