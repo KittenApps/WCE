935171ff2955f0db8d3011e5126ea4798232dca4
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-11 00:48:13 +0200

    split up functions/lockpickHelp

---
+++ src/functions/lockpickHelp.js
@@ -0,0 +1,49 @@
+import { SDK, HOOK_PRIORITIES } from '..';
+import { waitFor } from "../util/util";
+import { fbcSettings } from '../util/settings';
+import { debug } from '../util/logger';
+
+export async function lockpickHelp() {
+  await waitFor(() => !!StruggleMinigames);
+
+  /** @type {(s: number) => () => number} */
+  const newRand = (s) =>
+    function () {
+      s = Math.sin(s) * 10000;
+      return s - Math.floor(s);
+    };
+
+  const pinSpacing = 100,
+    pinWidth = 200,
+    x = 1575,
+    y = 300;
+
+  SDK.hookFunction(
+    "StruggleLockPickDraw",
+    HOOK_PRIORITIES.AddBehaviour,
+    /**
+     * @param {Parameters<typeof StruggleLockPickDraw>} args
+     */
+    (args, next) => {
+      if (fbcSettings.lockpick && StruggleLockPickOrder) {
+        const seed = parseInt(StruggleLockPickOrder.join(""));
+        const rand = newRand(seed);
+        const threshold = SkillGetWithRatio(Player, "LockPicking") / 20;
+        const hints = StruggleLockPickOrder.map((a) => {
+          const r = rand();
+          return r < threshold ? a : false;
+        });
+        for (let p = 0; p < hints.length; p++) {
+          // Replicates pin rendering in the game Struggle.js
+          const xx = x - pinWidth / 2 + (0.5 - hints.length / 2 + p) * pinSpacing;
+          if (hints[p] !== false) {
+            DrawText(`${StruggleLockPickOrder.indexOf(p) + 1}`, xx, y, "blue");
+          }
+        }
+      }
+      return next(args);
+    }
+  );
+  debug("hooking struggle for lockpick cheat draw", StruggleMinigames);
+  StruggleMinigames.LockPick.Draw = StruggleLockPickDraw;
+}
--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ import { commonPatches } from './functions/commonPatches';
 import { beepImprovements } from './functions/beepImprovements';
 import { commands } from './functions/commands';
 import { settingsPage } from './functions/settingsPage';
+import { lockpickHelp } from './functions/lockpickHelp';
 
 await waitFor(() => typeof FUSAM === "object" && FUSAM?.present && typeof bcModSdk === "object" && !!bcModSdk);
 
@@ -452,51 +453,6 @@ Player.FBC = FBC_VERSION;
 async function hookBCXAPI() {
   await waitFor(() => !!window.bcx);
   BCX = window.bcx?.getModApi("FBC") ?? null;
-}
-
-async function lockpickHelp() {
-  await waitFor(() => !!StruggleMinigames);
-
-  /** @type {(s: number) => () => number} */
-  const newRand = (s) =>
-    function () {
-      s = Math.sin(s) * 10000;
-      return s - Math.floor(s);
-    };
-
-  const pinSpacing = 100,
-    pinWidth = 200,
-    x = 1575,
-    y = 300;
-
-  SDK.hookFunction(
-    "StruggleLockPickDraw",
-    HOOK_PRIORITIES.AddBehaviour,
-    /**
-     * @param {Parameters<typeof StruggleLockPickDraw>} args
-     */
-    (args, next) => {
-      if (fbcSettings.lockpick && StruggleLockPickOrder) {
-        const seed = parseInt(StruggleLockPickOrder.join(""));
-        const rand = newRand(seed);
-        const threshold = SkillGetWithRatio(Player, "LockPicking") / 20;
-        const hints = StruggleLockPickOrder.map((a) => {
-          const r = rand();
-          return r < threshold ? a : false;
-        });
-        for (let p = 0; p < hints.length; p++) {
-          // Replicates pin rendering in the game Struggle.js
-          const xx = x - pinWidth / 2 + (0.5 - hints.length / 2 + p) * pinSpacing;
-          if (hints[p] !== false) {
-            DrawText(`${StruggleLockPickOrder.indexOf(p) + 1}`, xx, y, "blue");
-          }
-        }
-      }
-      return next(args);
-    }
-  );
-  debug("hooking struggle for lockpick cheat draw", StruggleMinigames);
-  StruggleMinigames.LockPick.Draw = StruggleLockPickDraw;
 }
 
 function automaticReconnect() {
