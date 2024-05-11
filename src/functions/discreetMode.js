ffbb4dd78e548b84b42f6217d88c156234499915
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-11 02:14:44 +0200

    split up functions/discreetMode

---
+++ src/functions/discreetMode.js
@@ -0,0 +1,71 @@
+import { SDK, HOOK_PRIORITIES } from "..";
+import { fbcSettings } from "../util/settings";
+import { isString } from "../util/util";
+import { displayText } from "../util/localization";
+
+export function discreetMode() {
+  /**
+   * @param {any} args
+   * @param {(args: any) => void} next
+   */
+  const discreetModeHook = (args, next) => {
+    if (fbcSettings.discreetMode) {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return next(args);
+  };
+
+  SDK.hookFunction("ChatRoomCharacterViewDrawBackground", HOOK_PRIORITIES.Top, discreetModeHook);
+
+  SDK.hookFunction("DrawCharacter", HOOK_PRIORITIES.Top, discreetModeHook);
+  SDK.hookFunction("NotificationDrawFavicon", HOOK_PRIORITIES.Top, discreetModeHook);
+
+  SDK.hookFunction(
+    "DrawImageEx",
+    HOOK_PRIORITIES.Top,
+    /**
+     * @param {Parameters<typeof DrawImageEx>} args
+     */
+    (args, next) => {
+      if (fbcSettings.discreetMode) {
+        if (!args) {
+          return false;
+        }
+        const isBackground = isString(args[0]) && args[0].startsWith("Backgrounds/");
+        const ignoredImages =
+          /(^Backgrounds\/(?!Sheet(White)?|grey|White\.|BrickWall\.)|\b(Kneel|Arousal|Activity|Asylum|Cage|Cell|ChangeLayersMouth|Diaper|Kidnap|Logo|Player|Remote|Restriction|SpitOutPacifier|Struggle|Therapy|Orgasm\d|Poses|HouseVincula|Seducer\w+)\b|^data:|^Assets\/(?!Female3DCG\/Emoticon\/(Afk|Sleep|Read|Gaming|Hearing|Thumbs(Up|Down))\/))/u;
+        if (isString(args[0]) && ignoredImages.test(args[0])) {
+          if (isBackground) {
+            args[0] = "Backgrounds/BrickWall.jpg";
+            return next(args);
+          }
+          return false;
+        }
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        if (args[0]?.src && ignoredImages.test(args[0].src)) {
+          return false;
+        }
+      }
+      return next(args);
+    }
+  );
+
+  SDK.hookFunction(
+    "NotificationTitleUpdate",
+    HOOK_PRIORITIES.Top,
+    /**
+     * @param {Parameters<typeof NotificationTitleUpdate>} args
+     */
+    (args, next) => {
+      if (fbcSettings.discreetMode) {
+        const notificationCount = NotificationGetTotalCount(1);
+        document.title = `${notificationCount > 0 ? `(${notificationCount}) ` : ""}${displayText("OnlineChat")}`;
+        return;
+      }
+      // eslint-disable-next-line consistent-return
+      return next(args);
+    }
+  );
+}
--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ import { leashFix } from './functions/leashFix';
 import { instantMessenger } from './functions/instantMessenger';
 import { extendedWardrobe } from './functions/extendedWardrobe';
 import { customContentDomainCheck } from './functions/customContentDomainCheck';
+import { discreetMode } from './functions/discreetMode';
 
 await waitFor(() => typeof FUSAM === "object" && FUSAM?.present && typeof bcModSdk === "object" && !!bcModSdk);
 
@@ -690,73 +691,6 @@ export function processChatAugmentsForLine(chatMessageElement, scrollToEnd) {
     chatMessageElement.appendChild(child);
   }
   chatMessageElement.setAttribute("bce-original-text", originalText);
-}
-
-function discreetMode() {
-  /**
-   * @param {any} args
-   * @param {(args: any) => void} next
-   */
-  const discreetModeHook = (args, next) => {
-    if (fbcSettings.discreetMode) {
-      return;
-    }
-    // eslint-disable-next-line consistent-return
-    return next(args);
-  };
-
-  SDK.hookFunction("ChatRoomCharacterViewDrawBackground", HOOK_PRIORITIES.Top, discreetModeHook);
-
-  SDK.hookFunction("DrawCharacter", HOOK_PRIORITIES.Top, discreetModeHook);
-  SDK.hookFunction("NotificationDrawFavicon", HOOK_PRIORITIES.Top, discreetModeHook);
-
-  SDK.hookFunction(
-    "DrawImageEx",
-    HOOK_PRIORITIES.Top,
-    /**
-     * @param {Parameters<typeof DrawImageEx>} args
-     */
-    (args, next) => {
-      if (fbcSettings.discreetMode) {
-        if (!args) {
-          return false;
-        }
-        const isBackground = isString(args[0]) && args[0].startsWith("Backgrounds/");
-        const ignoredImages =
-          /(^Backgrounds\/(?!Sheet(White)?|grey|White\.|BrickWall\.)|\b(Kneel|Arousal|Activity|Asylum|Cage|Cell|ChangeLayersMouth|Diaper|Kidnap|Logo|Player|Remote|Restriction|SpitOutPacifier|Struggle|Therapy|Orgasm\d|Poses|HouseVincula|Seducer\w+)\b|^data:|^Assets\/(?!Female3DCG\/Emoticon\/(Afk|Sleep|Read|Gaming|Hearing|Thumbs(Up|Down))\/))/u;
-        if (isString(args[0]) && ignoredImages.test(args[0])) {
-          if (isBackground) {
-            args[0] = "Backgrounds/BrickWall.jpg";
-            return next(args);
-          }
-          return false;
-        }
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        if (args[0]?.src && ignoredImages.test(args[0].src)) {
-          return false;
-        }
-      }
-      return next(args);
-    }
-  );
-
-  SDK.hookFunction(
-    "NotificationTitleUpdate",
-    HOOK_PRIORITIES.Top,
-    /**
-     * @param {Parameters<typeof NotificationTitleUpdate>} args
-     */
-    (args, next) => {
-      if (fbcSettings.discreetMode) {
-        const notificationCount = NotificationGetTotalCount(1);
-        document.title = `${notificationCount > 0 ? `(${notificationCount}) ` : ""}${displayText("OnlineChat")}`;
-        return;
-      }
-      // eslint-disable-next-line consistent-return
-      return next(args);
-    }
-  );
 }
 
 function autoStruggle() {
