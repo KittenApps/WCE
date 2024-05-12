7e56468afd623a2b69e50b6195b6c2accb0c71e3
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-12 16:49:50 +0200

    improve loaders

--- dist/wce-loader.user.js
+++ dist/wce-loader.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name WCE loader
 // @namespace https://www.bondageprojects.com/
-// @version 0.9
-// @description loader script for the fork of old fbc
-// @author Sidious (and others)
+// @version 1.0
+// @description Wholesome Club Extensions (WCE) - enhancements for the bondage club - fork of FBC 5.8
+// @author Sidious, Stella
+// @supportURL https://github.com/KittenApps/WCE
 // @match https://bondageprojects.elementfx.com/*
 // @match https://www.bondageprojects.elementfx.com/*
 // @match https://bondage-europe.com/*
 // @match https://www.bondage-europe.com/*
 // @match http://localhost:*/*
-// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @icon https://wce.netlify.app/icon.png
 // @grant none
 // @run-at document-end
 // ==/UserScript==
