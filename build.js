13a15e700cd792c947ec3953b66e85c0d62091d4
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-12 15:16:14 +0200

    add build script

---
+++ build.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import * as esbuild from 'esbuild'
+import { promises as fs } from 'node:fs';
+
+esbuild.build({
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  outfile: 'dist/wce.js',
+  format: 'esm',
+  sourcemap: 'linked'
+});
+
+if (process.env.NETLIFY) {
+  const URL = process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL;
+  const NAME = `@name WCE${process.env.BRANCH === 'main' ? '' : ' ' + process.env.BRANCH} loader`;
+  ['dist/wce-fusam-loader.user.js', 'dist/wce-loader.user.js'].map(f => fs.readFile(f, {encoding: 'utf8'}).then(
+    c => fs.writeFile(f, c.replace('http://localhost:4000', URL).replace('@name WCE loader', NAME))
+  ));
+}
