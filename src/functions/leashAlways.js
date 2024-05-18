34d3b12c22bcee95811110f7945e550f5b0a8ec3
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-18 20:32:08 +0200

    fix linter errors

--- src/functions/leashAlways.js
+++ src/functions/leashAlways.js
@@ -1,7 +1,7 @@
-import { waitFor, enableLeashing, disableLeashing } from "../util/utils";
+import { enableLeashing, disableLeashing } from "../util/utils";
 import { fbcSettings } from "../util/settings";
 
-export default async function leashAlways() {
+export default function leashAlways() {
   if (fbcSettings.leashAlways) {
     enableLeashing();
   } else {
