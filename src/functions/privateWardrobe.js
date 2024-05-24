da425700081c7cd8aed96284d8219c97b5db12d0
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-24 10:37:51 +0200

    local wardrobe

--- src/functions/privateWardrobe.js
+++ src/functions/privateWardrobe.js
@@ -197,7 +197,7 @@ export default async function privateWardrobe() {
         }
         args[0] = targetCharacter;
       }
-      if (fbcSettings.confirmWardrobeSave && Player.Wardrobe?.length > args[1] && Player.Wardrobe[args[1]].some((a) => a.Group === "Pronouns")) {
+      if (fbcSettings.confirmWardrobeSave && Player.Wardrobe?.length > args[1] && Player.Wardrobe[args[1]]?.some((a) => a.Group === "Pronouns")) {
         if (!window.confirm("Do you really want to override this wardrobe outfit?")) {
           return null;
         }
