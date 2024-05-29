5600b7caa8693691bc742ffcd6e488c5166feee9
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-05-29 19:02:13 +0200

    rolldown

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,9 +1,6 @@
-import esbuild from 'rollup-plugin-esbuild'
-import nodeResolve from '@rollup/plugin-node-resolve';
-import alias from '@rollup/plugin-alias';
 import { promises as fs } from 'node:fs';
 import LoaderBuilder from './loaderBuilder.js';
-import { defineConfig } from 'rollup'
+import { defineConfig, build } from 'rolldown';
 
 const LICENSE = `/**
 * @license GPL-3.0-or-later
@@ -22,36 +19,33 @@ const LICENSE = `/**
 *
 *  You should have received a copy of the GNU General Public License
 *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-`;
+*/`
 const loaderBuilder = new LoaderBuilder();
 
-export default defineConfig({
+const config = defineConfig({
   input: 'src/index.ts',
   output: { 
     dir: 'dist',
     entryFileNames: 'wce.js',
     chunkFileNames: '[name].js',
-    generatedCode: 'es2015',
+    // generatedCode: 'es2015',
     sourcemap: true,
-    banner: (chunk) => chunk.name === 'index' ? LICENSE : undefined,
+    banner: c => c.isEntry ? LICENSE : undefined,
+    minify: true,
+    target: 'es2022',
+    comments: 'preserve-legal',
+  },
+  resolve: {
+    conditionNames: ['import'],
+    alias: { 
+      'dexie': 'dexie/dist/modern/dexie.mjs',
+      'buttplug': 'buttplug/dist/web/buttplug.mjs',
+    },
+  },
+  define: {
+    PUBLIC_URL: `"${loaderBuilder.URL}"`,
   },
   plugins: [
-    alias({
-      entries: [
-        { find: 'buttplug', replacement: 'buttplug/dist/web/buttplug.mjs' },
-        { find: 'dexie', replacement: 'dexie/dist/modern/dexie.mjs' }
-      ],
-    }),
-    nodeResolve({ modulesOnly: true }),
-    esbuild({
-      sourceMap: true,
-      minify: true,
-      target: 'es2022',
-      define: {
-        PUBLIC_URL: `"${loaderBuilder.URL}"`,
-      },
-    }),
     {
       name: 'loader-builder-plugin',
       async buildStart() {
@@ -63,7 +57,16 @@ export default defineConfig({
         await Promise.all(['baby.png', 'icon.png', 'stutter.png'].map(fileName =>
           fs.readFile(`public/${fileName}`).then(source => this.emitFile({ type: 'asset', fileName, source }))
         ));
-      }
-    }
+      },
+    },
   ],
 });
+
+if (process.argv.includes('--watch')){
+  await build({ input: 'loaderBuilder.js', plugins: [{ generateBundle(_, b) { delete b['loaderBuilder.js']; } }, config.plugins[1]]});
+  config.plugins = [];
+  config.output.minify = false;
+  delete config.output.target;
+}
+
+export default config;
