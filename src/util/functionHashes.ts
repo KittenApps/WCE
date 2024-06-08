f8f64277b12908e702d574626c9886393d53a71a
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-06-08 14:24:33 +0200

    add settings for layeringHide + r105 compat

--- src/util/functionHashes.ts
+++ src/util/functionHashes.ts
@@ -153,7 +153,7 @@ export function expectedHashes(gameVersion: string): Readonly<Record<string, str
         ActivitySetArousal: "3AE28123",
         ActivitySetArousalTimer: "1342AFE2",
         ActivityTimerProgress: "6CD388A7",
-        AppearanceClick: "4E3937AA", // Screens/Character/Appearance/Appearance.js (13.5.2024)
+        AppearanceClick: "48B83523", // Screens/Character/Appearance/Appearance.js (21.5.2024)
         AppearanceLoad: "4360C485",
         AppearanceRun: "EAF1055D", // Screens/Character/Appearance/Appearance.js (13.5.2024)
         CharacterAppearanceBuildCanvas: "C05FE035", // Screens/Character/Appearance/Appearance.js
@@ -165,7 +165,7 @@ export function expectedHashes(gameVersion: string): Readonly<Record<string, str
         CharacterDelete: "57AA5D48",
         CharacterGetCurrent: "69F45A41",
         CharacterLoadCanvas: "EAB81BC4",
-        CharacterLoadOnline: "B1BCD3B1",
+        CharacterLoadOnline: "407FEDDE", // Scripts/Character.js
         CharacterNickname: "A794EFF5",
         CharacterRefresh: "301DA9CF",
         CharacterReleaseTotal: "BB9C6989",
@@ -174,28 +174,28 @@ export function expectedHashes(gameVersion: string): Readonly<Record<string, str
         CharacterSetActivePose: "566A14D7",
         ChatAdminRoomCustomizationClick: "9D859B28",
         ChatAdminRoomCustomizationProcess: "AF01C65A",
-        ChatRoomAppendChat: "998F2F98",
+        ChatRoomAppendChat: "318D7FC6", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomCharacterUpdate: "DE2DC592",
         ChatRoomCharacterViewDraw: "CA108410", // Screens/Online/ChatRoom/ChatRoomCharacterView.js (19.4.2024 )
         ChatRoomCharacterViewIsActive: "CD8066FA",
-        ChatRoomClearAllElements: "D67A7839",
-        ChatRoomClick: "747C51C4", // Screens/Online/ChatRoom/ChatRoom.js
+        ChatRoomClearAllElements: "629DEB2F", // Screens/Online/ChatRoom/ChatRoom.js
+        ChatRoomClick: "FF225068", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomCurrentTime: "A462DD3A",
-        ChatRoomDrawCharacterStatusIcons: "198C8657",
+        ChatRoomDrawCharacterStatusIcons: "B5209E11", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomGenerateChatRoomChatMessage: "6BFAC520", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomHideElements: "E1435FF3", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomHTMLEntities: "0A7ADB1D",
-        ChatRoomKeyDown: "CBE6830E",
+        ChatRoomKeyDown: "B00D692C", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomListManipulation: "75D28A8B",
         ChatRoomMapViewCharacterIsVisible: "286C447D",
         ChatRoomMapViewCharacterOnWhisperRange: "B0D08E96",
         ChatRoomMapViewIsActive: "D181020D",
         ChatRoomMenuBuild: "F76AEFC3",
-        ChatRoomMenuClick: "B1F7EBFB", // Screens/Online/ChatRoom/ChatRoom.js (6.5.2024)
+        ChatRoomMenuClick: "3BC3C582", // Screens/Online/ChatRoom/ChatRoom.js (6.5.2024)
         ChatRoomMenuDraw: "83275135", // Screens/Online/ChatRoom/ChatRoom.js (6.5.2024)
         ChatRoomMessage: "EA9F35DE", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomRegisterMessageHandler: "C432923A",
-        ChatRoomRun: "975B8ABD", // Screens/Online/ChatRoom/ChatRoom.js
+        ChatRoomRun: "BB97B6F1", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomSendChat: "21606D2F", // Screens/Online/ChatRoom/ChatRoom.js
         ChatRoomStart: "9B822A9A",
         CommandExecute: "C5477FA1", // Screens/Online/ChatRoom/Commands.js
@@ -208,7 +208,7 @@ export function expectedHashes(gameVersion: string): Readonly<Record<string, str
         DialogCanUnlock: "A86B2558",
         DialogDrawItemMenu: "FCE556C2",
         DialogLeave: "C37553DC",
-        DialogMenuButtonBuild: "AC1C6466", // Scripts/Dialog.js (13.5.2024)
+        DialogMenuButtonBuild: "3FDE2F94", // Scripts/Dialog.js (13.5.2024)
         DialogMenuButtonClick: "C1F5E0AF", // Scripts/Dialog.js (2.5.2024)
         DrawArousalMeter: "BB0755AF",
         DrawArousalThermometer: "7ED6D822",
@@ -223,7 +223,7 @@ export function expectedHashes(gameVersion: string): Readonly<Record<string, str
         DrawText: "C1BF0F50",
         DrawTextFit: "F9A1B11E",
         ElementCreate: "C2D64500",
-        ElementCreateTextArea: "AA4AEDE7",
+        ElementCreateTextArea: "4E040819", // Scripts/Element.js
         ElementIsScrolledToEnd: "1CC4FE11",
         ElementPosition: "3D9F7B72", // Scripts/Element.js
         ElementScrollToEnd: "1AC45575",
@@ -231,7 +231,7 @@ export function expectedHashes(gameVersion: string): Readonly<Record<string, str
         FriendListShowBeep: "6C0449BB",
         GameRun: "337CB358", // Scripts/Game.js
         GLDrawResetCanvas: "81214642",
-        InformationSheetRun: "060A6DBA", // Screens/Character/InformationSheet/InformationSheet.js
+        InformationSheetRun: "F5D94308", // Screens/Character/InformationSheet/InformationSheet.js
         InterfaceTextGet: "66603471", // Scripts/Text.js (2.5.2024)
         InventoryGet: "E666F671",
         "Layering.Load": "EF69E984",
