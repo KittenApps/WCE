62a997a3a3aa68763de2a76355b66696beecc49c
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-06-15 13:30:04 +0200

    ci: run es-check es2022 on dist

--- eslint.config.js
+++ eslint.config.js
@@ -6,12 +6,7 @@ import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  stylistic.configs.customize({
-    quotes: 'double',
-    semi: true,
-    jsx: false,
-    braceStyle: '1tbs',
-  }),
+  stylistic.configs.customize({ quotes: "double", semi: true, jsx: false, braceStyle: "1tbs" }),
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
