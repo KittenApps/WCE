b6967a1d81d46e70cedbb90433c85aa80d57ed5b
Silizia <66546362+KittenApps@users.noreply.github.com>
2024-08-24 16:07:26 +0200

    generate loaders without copying and replacing files

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import esbuild from 'rollup-plugin-esbuild'
 import nodeResolve from '@rollup/plugin-node-resolve';
-import copy from 'rollup-plugin-copy';
 import alias from '@rollup/plugin-alias';
 import { promises as fs } from 'node:fs';
+import LoaderBuilder from './loaderBuilder.js';
 
 await fs.rm('dist', { recursive: true, force: true });
+await fs.mkdir('dist');
 
 const LICENSE = `/**
 * @license GPL-3.0-or-later
@@ -25,13 +26,7 @@ const LICENSE = `/**
 *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 `;
-const MATCH = `// @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.elementfx.com/*
-// @match https://bondage-europe.com/*
-// @match https://www.bondage-europe.com/*
-// @match https://bondageprojects.com/*
-// @match https://www.bondageprojects.com/*`;
-const URL = process.env.CONTEXT === 'production' ? process.env.URL : process.env.DEPLOY_PRIME_URL;
+const loaderBuilder = new LoaderBuilder();
 
 export default {
   input: 'src/index.ts',
@@ -56,48 +51,14 @@ export default {
       minify: true,
       target: 'es2022',
       define: {
-        PUBLIC_URL: URL ? `"${URL}"` : '"http://localhost:4000"',
+        PUBLIC_URL: `"${loaderBuilder.URL}"`,
       },
     }),
-    copy({
-      targets: [
-        { src: 'public/*.png', dest: 'dist' },
-        ...(process.env.NETLIFY && (process.env.BRANCH === 'main' || process.env.BRANCH === 'beta') ? [{ 
-            src: 'public/wce-fusam-loader.user.js',
-            dest: 'dist',
-            transform: (contents) => contents.toString()
-              .replace('@name WCE loader', `@name WCE${process.env.BRANCH === 'main' ? '' : ' ' + process.env.BRANCH} loader`)
-              .replace('??= "stable";', `??= "${process.env.BRANCH === 'main' ? 'stable' : 'dev'}";`)
-          }] : [{ 
-            src: 'public/wce-local-loader.user.js',
-            dest: 'dist',
-            rename: 'wce-fusam-loader.user.js',
-            transform: (contents) => {
-              if (process.env.NETLIFY) {
-                return contents.toString()
-                  .replace('http://localhost:4000', URL)
-                  .replace('@name WCE local loader', `@name WCE PR #${process.env.REVIEW_ID} loader`)
-                  .replace('// @match http://localhost:*/*', MATCH);
-              }
-              return contents
-            }
-          }]
-        ),
-        { 
-          src: 'public/wce-loader.user.js',
-          dest: 'dist',
-          transform: (contents) => {
-            if (process.env.NETLIFY) {
-              const BRANCH = process.env.BRANCH === 'main' ? '' : (process.env.BRANCH === 'beta' ? ' beta': ` PR #${process.env.REVIEW_ID}`);
-              return contents.toString()
-                .replace('http://localhost:4000', URL)
-                .replace('@name WCE local loader', `@name WCE${BRANCH} loader`)
-                .replace('// @match http://localhost:*/*', MATCH);
-            }
-            return contents
-          }
-        }
-      ]
-    })
   ],
 }
+
+for (const f of ['baby.png', 'icon.png', 'stutter.png']) {
+  fs.copyFile(`public/${f}`, `dist/${f}`);
+}
+fs.writeFile('dist/wce-fusam-loader.user.js', loaderBuilder.generateFusamLoader());
+fs.writeFile('dist/wce-loader.user.js', loaderBuilder.generateStandaloneLoader());
