07fb1bdcdacfa8e3ddacd593e3ac0f8a040fa02b
Silizia <66546362+KittenApps@users.noreply.github.com>
2025-04-07 23:03:22 +0200

    fix

--- src/functions/privateWardrobe.js
+++ src/functions/privateWardrobe.js
@@ -100,7 +100,8 @@ export default async function privateWardrobe() {
       if (inCustomWardrobe) {
         // @ts-ignore - explicitly overriding with another Character temporarily
         Player = targetCharacter;
-        Player.VisualSettings.ForceFullHeight = false;
+        // @ts-ignore
+        Player.VisualSettings = { ForceFullHeight: false };
       }
       const ret = next(args);
       if (inCustomWardrobe) {
